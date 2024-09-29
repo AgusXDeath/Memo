3d466305eef0446d6f5b61461c253a54b603b106
Xavier <45172320+JavierJr30789@users.noreply.github.com>
2024-09-29 15:06:24 -0300

    Subiendo la api
    Consideraciones si quiere hacer funcionar el proyecto de angular con esta api, la carpeta api-php tiene que estar si o si en el htdocs, o la capeta la cual corra el servidor apache

---
+++ api-php/core/Router.php
@@ -0,0 +1,100 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization");
+// app/core/RouterUsuariosGrupos.php
+
+include_once '../controllers/UsuariosGruposController.php';
+include_once '../views/View.php';
+
+$controller = new UsuariosGruposController();
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Verificar si 'resource' está definido en la URL
+if (isset($_GET['resource'])) {
+    $resource = $_GET['resource'];  // Puede ser 'usuarios', 'grupos', 'funciones', 'grupofunciones'
+
+    switch ($method) {
+        case 'GET':
+            if ($resource === 'usuarios') {
+                if (isset($_GET['id'])) {
+                    $result = $controller->getUsuarioById($_GET['id']);
+                } else {
+                    $result = $controller->getAllUsuarios();
+                }
+            } elseif ($resource === 'grupos') {
+                if (isset($_GET['id'])) {
+                    $result = $controller->getGrupoById($_GET['id']);
+                } else {
+                    $result = $controller->getAllGrupos();
+                }
+            } elseif ($resource === 'funciones') {
+                if (isset($_GET['id'])) {
+                    $result = $controller->getFuncionById($_GET['id']);
+                } else {
+                    $result = $controller->getAllFunciones();
+                } 
+            } elseif ($resource === 'grupofunciones') {
+                if (isset($_GET['id'])) {
+                    $result = $controller->getgrupoFuncionesById($_GET['id']);
+                } else {
+                    $result = $controller->getAllgrupoFunciones();
+                } 
+            }
+            View::render($result);
+            break;
+
+        case 'POST':
+            $data = json_decode(file_get_contents("php://input"));
+            if ($resource === 'usuarios') {
+                $result = $controller->createUsuario($data);
+            } elseif ($resource === 'grupos') {
+                $result = $controller->createGrupo($data);
+            } elseif ($resource === 'funciones') {
+                $result = $controller->createFuncion($data);
+            } elseif ($resource === 'gruposFunciones') {
+                $result = $controller->creategrupoFunciones($data);
+            }
+            View::render($result);
+            break;
+
+        case 'PUT':
+            $data = json_decode(file_get_contents("php://input"));
+            if (isset($_GET['id'])) {
+                if ($resource === 'usuarios') {
+                    $result = $controller->updateUsuario($_GET['id'], $data);
+                } elseif ($resource === 'grupos') {
+                    $result = $controller->updateGrupo($_GET['id'], $data);
+                } elseif ($resource === 'funciones') {
+                    $result = $controller->updateFuncion($_GET['id'], $data);
+                } elseif ($resource === 'gruposFunciones') {
+                    $result = $controller->updategrupoFunciones($_GET['id'],$data);
+                }
+                View::render($result);
+            }
+            break;
+
+        case 'DELETE':
+            if (isset($_GET['id'])) {
+                if ($resource === 'usuarios') {
+                    $result = $controller->deleteUsuario($_GET['id']);
+                } elseif ($resource === 'grupos') {
+                    $result = $controller->deleteGrupo($_GET['id']);
+                } elseif ($resource === 'funciones') {
+                    $result = $controller->deleteFuncion($_GET['id']);
+                } elseif ($resource === 'gruposFunciones') {
+                    $result = $controller->deletegrupoFunciones($_GET['id']);
+                }
+                View::render($result);
+            }
+            break;
+
+        default:
+            View::render(json_encode(["message" => "Método no permitido"]));
+            break;
+    }
+} else {
+    // Manejo del caso en que 'resource' no está presente en la URL
+    View::render(json_encode(["message" => "Recurso no especificado en la URL"]));
+}
+?>

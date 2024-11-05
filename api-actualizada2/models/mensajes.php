19c68fba02e6b87c73541ee146e541e292238bb8
Xavier <45172320+JavierJr30789@users.noreply.github.com>
2024-11-05 18:15:49 -0300

    api subida 2

---
+++ api-actualizada2/models/mensajes.php
@@ -0,0 +1,63 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization");
+// app/models/Mensaje.php
+
+class Mensajes {
+    private $conn;
+    private $table = "mensajes";
+
+    public $idMensajes;
+    public $emisor;
+    public $receptor;
+    public $mensaje;
+    public $estadoLeido;
+    public $estadoEnviado;
+    public $estadoFavorito;
+    public $estadoPapelera;
+    public $estadoRecibido;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Obtener todas las relaciones grupo-funciones
+    public function getAll() {
+        $query = "SELECT * FROM " . $this->table;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    // Obtener una relación por ID
+    public function getById($id) {
+        $query = "SELECT * FROM " . $this->table . " WHERE idMensajes = :idMensajes";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":idMensajes", $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Eliminar un mensaje por ID
+    public function deleteMensaje($id) {
+        $query = "DELETE FROM " . $this->table . " WHERE idMensajes = :idMensajes";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":idMensajes", $id);
+        return $stmt->execute();
+    }
+
+    
+   // Editar un mensaje por ID
+   public function updateMensaje($id, $contenido, $estadoFavorito, $estadoPapelera) {
+    $query = "UPDATE " . $this->table . " SET mensaje = :mensaje, estadoFavorito = :estadoFavorito, estadoPapelera = :estadoPapelera WHERE idMensajes = :idMensajes";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(":mensaje", $contenido);
+    $stmt->bindParam(":estadoFavorito", $estadoFavorito, PDO::PARAM_INT);
+    $stmt->bindParam(":estadoPapelera", $estadoPapelera, PDO::PARAM_INT);
+    $stmt->bindParam(":idMensajes", $id);
+    return $stmt->execute();
+}
+
+}
+?>
